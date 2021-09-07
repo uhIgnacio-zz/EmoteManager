3f733099742d2768ef5138c716b816dac978777f
igna <igna@hypixel.one>
2021-09-07 15:14:44 +0000

    format

--- data/config.example.py
+++ data/config.example.py
@@ -23,7 +23,7 @@
                 'guilds': [
                 ],
             },
-        },
+    },
 
     'copyright_license_file': 'data/short-license.txt',
 
@@ -46,8 +46,8 @@
         # so that both the staging and the stable versions of the bot can use them
         'response_emojis': {
             'success': {  # emotes used to indicate success or failure
-                False: '', # <:EmoteName:ID>
-                True: '' # <:EmoteName:ID>
+                False: '',  # <:EmoteName:ID>
+                True: ''  # <:EmoteName:ID>
             },
-        },
+    },
 }
--- utils/misc.py
+++ utils/misc.py
@@ -5,47 +5,51 @@
 
 import asyncio
 
-import discord
+import nextcord
+
 
 def format_user(user, *, mention=False):
-	"""Format a user object for audit log purposes."""
-	# not mention: @null byte#8191 (140516693242937345)
-	# mention: <@140516693242937345> (null byte#8191)
-	# this allows people to still see the username and discrim
-	# if they don't share a server with that user
-	if mention:
-		return f'{user.mention} (@{user})'
-	else:
-		return f'@{user} ({user.id})'
+    """Format a user object for audit log purposes."""
+    # not mention: @null byte#8191 (140516693242937345)
+    # mention: <@140516693242937345> (null byte#8191)
+    # this allows people to still see the username and discrim
+    # if they don't share a server with that user
+    if mention:
+        return f'{user.mention} (@{user})'
+    else:
+        return f'@{user} ({user.id})'
 
-def format_http_exception(exception: discord.HTTPException):
-	"""Formats a discord.HTTPException for relaying to the user.
-	Sample return value:
 
-	BAD REQUEST (status code: 400):
-	Invalid Form Body
-	In image: File cannot be larger than 256 kb.
-	"""
-	return (
-		f'{exception.response.reason} (status code: {exception.response.status}):'
-		f'\n{exception.text}')
+def format_http_exception(exception: nextcord.HTTPException):
+    """Formats a nextcord.HTTPException for relaying to the user.
+    Sample return value:
+
+    BAD REQUEST (status code: 400):
+    Invalid Form Body
+    In image: File cannot be larger than 256 kb.
+    """
+    return (
+        f'{exception.response.reason} (status code: {exception.response.status}):'
+        f'\n{exception.text}')
+
 
 def strip_angle_brackets(string):
-	"""Strip leading < and trailing > from a string.
-	Useful if a user sends you a url like <this> to avoid embeds, or to convert emotes to reactions."""
-	if string.startswith('<') and string.endswith('>'):
-		return string[1:-1]
-	return string
+    """Strip leading < and trailing > from a string.
+    Useful if a user sends you a url like <this> to avoid embeds, or to convert emotes to reactions."""
+    if string.startswith('<') and string.endswith('>'):
+        return string[1:-1]
+    return string
+
 
 async def gather_or_cancel(*awaitables, loop=None):
-	"""run the awaitables in the sequence concurrently. If any of them raise an exception,
-	propagate the first exception raised and cancel all other awaitables.
-	"""
-	gather_task = asyncio.gather(*awaitables, loop=loop)
-	try:
-		return await gather_task
-	except asyncio.CancelledError:
-		raise
-	except:
-		gather_task.cancel()
-		raise
+    """run the awaitables in the sequence concurrently. If any of them raise an exception,
+    propagate the first exception raised and cancel all other awaitables.
+    """
+    gather_task = asyncio.gather(*awaitables, loop=loop)
+    try:
+        return await gather_task
+    except asyncio.CancelledError:
+        raise
+    except:
+        gather_task.cancel()
+        raise
